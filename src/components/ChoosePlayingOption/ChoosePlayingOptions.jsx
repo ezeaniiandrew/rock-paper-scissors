635edf77ac2e28cfb5b9c0f7cfe3a6dec134fcda
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-29 22:07:09 +0100

    created new conponent to display available game options

---
+++ src/components/ChoosePlayingOption/ChoosePlayingOptions.jsx
@@ -0,0 +1,45 @@
+import { createPortal } from "react-dom";
+import { useGame } from "../../context";
+import { actionTypes } from "../../reducers/gameReducer";
+import BonusGame from "../BonusGame.jsx/BonusGame";
+import ClassicGame from "../ClassicGame/ClassicGame";
+import Modal from "../Modal/Modal";
+import styles from "./choose-playing-option.module.css";
+import Button from "../Button/Button";
+import { useEffect } from "react";
+
+function ChoosePlayingOption() {
+  const { gameMode, isModalOpen, dispatch } = useGame();
+
+  const handleClick = (mode) => {
+    if (mode === "classic") {
+      dispatch({ type: actionTypes.CHANGE_GAME_MODE, payload: "bonus" });
+    } else {
+      dispatch({ type: actionTypes.CHANGE_GAME_MODE, payload: "classic" });
+    }
+  };
+
+  useEffect(() => {
+    const savedScore = JSON.parse(localStorage.getItem("score")) || 0;
+
+    dispatch({ type: actionTypes.SET_SCORE, payload: savedScore });
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {gameMode === "classic" ? <ClassicGame /> : <BonusGame />}
+      {isModalOpen &&
+        createPortal(<Modal />, document.getElementById("portal-root"))}
+      <div className={styles.btn_box}>
+        <Button onClick={() => handleClick(gameMode)}>
+          {gameMode === "classic" ? "Bonus" : "Classic"}
+        </Button>
+        <Button onClick={() => dispatch({ type: actionTypes.SET_SHOW_RULES })}>
+          Rules
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ChoosePlayingOption;
